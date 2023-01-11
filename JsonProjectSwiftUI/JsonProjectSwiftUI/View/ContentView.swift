ad7047c833e34b92cf47a8985fdc1e2a133cb7e8
vijaya shree kanwal <56352158+Vijaya9418@users.noreply.github.com>
2023-01-11 16:56:01 +0530

    JSON parsing using SwiftUI

---
+++ JsonProjectSwiftUI/JsonProjectSwiftUI/View/ContentView.swift
@@ -0,0 +1,38 @@
+import SwiftUI
+import Foundation
+
+
+
+
+
+struct ContentView: View {
+    @StateObject var viewModel = NewsViewModel()
+    var body: some View {
+        NavigationView{
+            List{
+                ForEach(viewModel.news, id: \.self) {
+                    course in
+                    NavigationLink(destination: DetailView(url: course.url)){
+                        HStack{
+                            Text("\(course.points!)")
+                            Text(course.title!)
+                        }
+                        .padding(3)
+                    }
+                }
+                    
+            }
+            .navigationTitle("Courses")
+            .onAppear{
+                viewModel.fetch()
+            }
+        }
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
